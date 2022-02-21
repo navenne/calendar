c67fe4a594e1f792d8b5f45c81eca3148ee7d534
Laura <a20hirila@iesgrancapitan.org>
2022-02-21 13:40:57 +0100

    adding to portfolio

---
+++ arrays.php
@@ -0,0 +1,49 @@
+<?php
+/**
+* @author Laura Hidalgo Rivera
+* 
+*/
+
+$months = array(
+    array("value" => 1, "literal" => "Enero"),
+    array("value" => 2, "literal" => "Febrero"),
+    array("value" => 3, "literal" => "Marzo"),
+    array("value" => 4, "literal" => "Abril"),
+    array("value" => 5, "literal" => "Mayo"),
+    array("value" => 6, "literal" => "Junio"),
+    array("value" => 7, "literal" => "Julio"),
+    array("value" => 8, "literal" => "Agosto"),
+    array("value" => 9, "literal" => "Septiembre"),
+    array("value" => 10, "literal" => "Octubre"),
+    array("value" => 11, "literal" => "Noviembre"),
+    array("value" => 12, "literal" => "Diciembre")
+);
+
+$holidays = array(
+    array("day" => 1, "month" => 1, "type" => "national"),
+    array("day" => 6, "month" => 1, "type" => "national"),
+    array("day" => 28, "month" => 2, "type" => "comm"),
+    array("day" => 1, "month" => 4, "type" => "comm"),
+    array("day" => 2, "month" => 4, "type" => "national"),
+    array("day" => 1, "month" => 5, "type" => "national"),
+    array("day" => 15, "month" => 8, "type" => "comm"),
+    array("day" => 8, "month" => 9, "type" => "local"),
+    array("day" => 12, "month" => 10, "type" => "national"),
+    array("day" => 25, "month" => 10, "type" => "local"),
+    array("day" => 1, "month" => 11, "type" => "national"),
+    array("day" => 6, "month" => 12, "type" => "national"),
+    array("day" => 8, "month" => 12, "type" => "national"),
+    array("day" => 25, "month" => 12, "type" => "national")
+);
+
+$initials = array("L", "M", "X", "J", "V", "S", "D");
+
+$weekdays = array(
+    "Monday" => 1,
+    "Tuesday" => 2,
+    "Wednesday" => 3,
+    "Thursday" => 4,
+    "Friday" => 5,
+    "Saturday" => 6,
+    "Sunday" => 7
+);
---
+++ calendario.php
@@ -0,0 +1,166 @@
+<?php
+
+/**
+ * @author Laura Hidalgo Rivera
+ * 
+ * Dado el mes y año almacenados en variables, escribir un programa que muestre el calendario mensual
+ * correspondiente. Marcar el día actual en verde y los festivos en rojo.
+ */
+
+include "arrays.php";
+
+$monthSelected = date('m');
+$yearSelected = date('Y');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["submit"])) {
+        if (isset($_POST["month"])) {
+            $monthSelected = $_POST["month"];
+        }
+
+        if (isset($_POST["year"])) {
+            $yearSelected = $_POST["year"];
+        }
+    }
+}
+
+$firstDay = date("l", mktime(0, 0, 0, date("$monthSelected"), 1, date("$yearSelected")));
+$firstDayPosition = $weekdays[$firstDay];
+$day = 0;
+$dayNextMonth = 1;
+$today = date('d');
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="Laura Hidalgo Rivera">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calendario</title>
+    <link rel="stylesheet" href="calendario.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+</head>
+
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th colspan="7">
+                    <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+                        <select name="month">
+                            <?php
+                            foreach ($months as $key => $value) {
+                                $selected = ($monthSelected == $value["value"]) ? 'selected' : '';
+                                echo "<option value = \"" . $value["value"] . "\" $selected >" . $value["literal"] . "</option>";
+                            }
+                            ?>
+                        </select>
+                        <input type="number" name="year" min="1922" max="2122" value="<?= $yearSelected; ?>">
+                        <input type="submit" name="submit" value="&#10003;" id="submit">
+                    </form>
+                </th>
+            </tr>
+            <tr>
+                <?php
+                foreach ($initials as $key) {
+                    echo "<th>$key</th>";
+                }
+                ?>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            for ($i = 0; $i < 5; $i++) {
+                echo "<tr>";
+                for ($j = 1; $j < 8; $j++) {
+                    echo "<td";
+                    if ($i == 0 && $j == $firstDayPosition) {
+                        $day++;
+                    }
+                    if (isHoliday($day, $monthSelected, $holidays)) {
+                        echo(" class=\"" . typeOfHoliday($day, $monthSelected, $holidays) . "\"");
+                    }
+                    if ($day == $today && $monthSelected == date('m')) {
+                        echo " id=\"today\"";
+                    }
+                    if ($j == 7) {
+                        echo (isNextMonth($day, $monthSelected, $yearSelected) ? " class=\"nextMonth\"" : " class=\"sunday\"");
+                    }
+                    if (isNextMonth($day, $monthSelected, $yearSelected)) {
+                        echo " class=\"nextMonth\">$dayNextMonth</td>";
+                        $dayNextMonth++;
+                    } elseif ($i == 0 && $j < $firstDayPosition) {
+                        echo "></td>";
+                    } else {
+                        echo ">$day</td>";
+                        $day++;
+                    }
+                }
+                echo "</tr>";
+            }
+            ?>
+        </tbody>
+    </table>
+    <div class="legend">
+        <p class="sunday">Domingo</p>
+        <p class="national">Festivo nacional</p>
+        <p class="comm">Festivo Comunidad</p>
+        <p class="local">Festivo local</p>
+    </div>
+</body>
+
+</html>
+
+<?php
+
+
+/**
+ * Devuelve el número de días del mes indicado por parámetro
+ */
+function daysInMonth($month, $year)
+{
+    return cal_days_in_month(CAL_GREGORIAN, $month, $year);
+}
+
+/**
+ * Devuelve si el día corresponde al mes siguiente
+ */
+function isNextMonth($day, $month, $year)
+{
+    return $day > daysInMonth($month, $year);
+}
+
+function isHoliday($day, $month, $holidays)
+{
+    foreach ($holidays as $key => $value) {
+        if ($month == $value['month'] && $day == $value['day']) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function typeOfHoliday($day, $month, $holidays)
+{
+    foreach ($holidays as $key => $value) {
+        if ($month == $value['month'] && $day == $value['day']) {
+            switch ($value['type']) {
+                case 'national':
+                    return "national";
+                case 'comm':
+                    return "comm";
+                case 'local':
+                    return "local";
+                default:
+                    return;
+            }
+        }
+    }
+    return;
+}
+
+?>
